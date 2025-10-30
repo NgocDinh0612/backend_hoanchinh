f2f2404ac7e63f398a549546a8f172b1f716b1c9
NgocDinh0612 <nguyenngocdinh0612@gmail.com>
2025-10-30 10:45:24 +0700

    update models/schedule

--- src/models/Schedule.js
+++ src/models/Schedule.js
@@ -1,10 +1,14 @@
 const mongoose = require('mongoose');
 
 const scheduleSchema = new mongoose.Schema({
-  startTime: { type: String, required: true },   // "HH:mm"
-  endTime: { type: String, required: true },     // "HH:mm"
+  gatewayId: { type: String, required: true },
+  deviceId: { type: String, required: true },
+  brightness: { type: Number, min: 0, max: 100, required: true },
+  startTime: { type: String, required: true }, // "HH:mm:ss"
+  endTime: { type: String, required: true },   // "HH:mm:ss"
   action: { type: String, enum: ['on', 'off'], required: true },
-  daysOfWeek: [{ type: Number }],                // 0 = CN, ..., 6 = T7
+  daysOfWeek: [{ type: Number }],              // 0 = CN, ..., 6 = T7
+  repeat: { type: String, enum: ['none', 'daily', 'weekly', 'monthly'], default: 'none' },
   createdAt: { type: Date, default: Date.now }
 });
 
