495db435d8b37c4a73d82dff065058ecf038a733
NgocDinh0612 <nguyenngocdinh0612@gmail.com>
2025-11-16 11:18:20 +0700

    Update and rename mailer.js to resendMailer.js

---
+++ src/utils/resendMailer.js
@@ -0,0 +1,21 @@
+// utils/resendMailer.js
+const { Resend } = require('resend');
+const resend = new Resend(process.env.RESEND_API_KEY);
+
+async function sendResendEmail({ to, subject, html, text }) {
+  try {
+    await resend.emails.send({
+      from: 'Smart Streetlight <onboarding@resend.dev>',
+      to: [to],
+      subject,
+      html,
+      text: text || html.replace(/<[^>]*>/g, ''), // fallback text
+    });
+    console.log(`Email sent to ${to}`);
+  } catch (error) {
+    console.error('Resend email error:', error);
+    throw error;
+  }
+}
+
+module.exports = { sendResendEmail };
